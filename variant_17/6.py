e564485a01b301080bf487c60d0dcbe1a8bf965a
kos <liskosnik@yandex.ru>
2021-03-11 17:44:03 +0300

    на уроке вариант 17

--- variant_17/6.py
+++ variant_17/6.py
@@ -2,10 +2,10 @@ def f(s):
     n = 200
     while s // n >= 2:
         s = s + 5
-    n = n + 5
-    return
+        n = n + 5
+    return s
 
 
 for i in range(1, 100000):
-    if f(i) in range(100, 1000):
+    if 100 <= f(i) <= 999:
         print(i)
